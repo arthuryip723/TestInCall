cfdb34174921442258958ca3099c6dba15198015
Arthur Yip <arthuryip723@gmail.com>
2017-01-30 22:45:36 -0500

    intermediate work

--- models/review.js
+++ models/review.js
@@ -3,6 +3,11 @@ var mongoose = require('mongoose')
 
 var reviewSchema = new Schema({
   rating: {type: Number, min: 1, max: 5, default: 3},
+  // faceRating: {type: Number, min: 1, max: 5, default: 3},
+  // figureRating: {type: Number, min: 1, max: 5, default: 3},
+  // serviceRating: {type: Number, min: 1, max: 5, default: 3},
+  // priceRating: {type: Number, min: 1, max: 5, default: 3},
+  // pprRating: {type: Number, min: 1, max: 5, default: 3},
   content: String,
   person: {
     type: Schema.Types.ObjectId,
